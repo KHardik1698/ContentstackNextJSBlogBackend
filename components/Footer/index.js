33a430d924226e0ceb2e6adc15963f9e31e05c9a
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-21 21:01:10 +0530

    Added Footer Styling
    Added Flex to Footer Component

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import styles from "./index.module.css";
 
 function Footer(props) {
   let footer = props.footer[0];
   return (
-    <div>
+    <div className={styles["footer-container"]}>
       <p>{footer.footer_title}</p>
       <p>{footer.copyright_text}</p>
     </div>
