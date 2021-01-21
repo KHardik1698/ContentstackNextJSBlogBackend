aa83ea65e8769dd858d79ffb44b6338f977321a3
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-21 19:55:12 +0530

    Added Header Styling
    Added Flex to Header Component, adjusted size of various Header Components

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import styles from "./index.module.css";
 
 function Header(props) {
   let header = props.header[0];
   return (
-    <div>
-      <img src={header.header_logo.url} alt="Logo" />
-      <p>{header.team_name}</p>
-      <div>
-        {header.navigation_links.map((link) => {
+    <div className={styles["header-container"]}>
+      <div className={styles["header-logo-container"]}>
+        <img className={styles["header-logo"]} src={header.header_logo.url} alt="Logo" />
+      </div>
+      <p className={styles["team-name"]}>{header.team_name}</p>
+      <div className={styles["navigation-links"]}>
+        {header.navigation_links.map((link, i) => {
           return (
-            <div>
+            <div key={i}>
               <Link href={link.href}>
                 <p>{link.title}</p>
               </Link>
