5707d7bd866fa145cfb1c0fd2f243622549773a5
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-21 13:10:01 +0530

    Rendered Blogs Title on All Blogs Page

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,19 +1,26 @@
 import fetchData from "../../fetchData";
 
 function blogs(props) {
-  //   console.log(props.blogs);
+  let blogs = props.allBlogs;
   return (
     <div>
-      <h1>All Blogs</h1>
+      <h1>Sports Blog</h1>
+      {blogs.map((blog, i) => {
+        return (
+          <div key={i}>
+            <p>{blog.blog_title}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  let allBlogs = await fetchData({ contentType: "" });
+  let allBlogs = await fetchData({ contentType: "blog_rendering_backend" });
   return {
     props: {
-      allBlogs: { ...allBlogs },
+      allBlogs: [...allBlogs],
     },
   };
 };
